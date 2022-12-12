64728b2ff8eabaabba810a76dc225b798add6926
Paul Biber <roadblock37@gmail.com>
2022-12-12 14:38:25 -0500

    font awesome setup and icons imported in navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,8 +1,35 @@
+import React, {useState} from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import logo from '../images/logo.svg'
+// array of links to populate navbar dynamically
+const links = [
+    {id: 1, link: "Home"},
+    {id: 2, link: "About"},
+    {id: 3, link: "Contact"},
+    {id: 4, link: "Blog"},
+    {id: 5, link: "Careers"},
 
+];
 
 const NavBar = () => {
+    const [showLinks, setShowLinks] = useState(false);
+
+    // toggle for mobile nav to show links
+    const toggleLinks = () => {
+        setShowLinks(!showLinks);
+    }
+
+
     return (
-        <h1>Nav bar</h1>
+        <nav>
+            <div className='nav-header'>
+                <img src={logo} alt='logo' className='logo'/>
+                <button className='nav-toggle' onClick={toggleLinks}>
+                    <FontAwesomeIcon icon={faBars}/>
+                </button>
+            </div>
+        </nav>
     );
 }
 
