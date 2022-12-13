c5e040f45508b84308a3db73affb2f660a88b699
Paul Biber <roadblock37@gmail.com>
2022-12-13 10:01:27 -0500

    basic styling for nav bar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -42,7 +42,7 @@ const NavBar = () => {
         <nav>
             <div className='nav-header'>
                 <img src={logo} alt='logo' className='logo'/>
-                <button className='nav-toggle' onClick={toggleLinks}>
+                <button id='nav-toggle' onClick={toggleLinks}>
                     
                 {showLinks? <FontAwesomeIcon icon={faXmark}/>: <FontAwesomeIcon icon={faBars}/> }
                 </button>
